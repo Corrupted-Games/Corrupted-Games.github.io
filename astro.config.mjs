df0f95a83a21a0dbe6123a3fbff76a5b6236dc64
Sinclair <truly.sinclair@gmail.com>
2024-06-02 20:48:24 -0400

    Integrate React into the project
    React has been added as a new dependency into the astro.config.mjs file. Additionally, several necessary packages for React such as "@astrojs/react", "react" and "react-dom" have been added to the package.json file. Lastly, JSX support for React has been enabled in the TypeScript config.
    
    Signed-off-by: Sinclair <truly.sinclair@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://corrupted-games.github.io/",
-  base:'/corrupted-games'
+  site: "https://corrupted-games.github.io/",
+  base: '/corrupted-games',
+  integrations: [react()]
 });
